7c6629b64d20a70c7eddf49f9017e7446ba33cf7
Michael Rubinstein <michael@polyblog.com>
2016-09-27 21:05:55 -0700

    Implemented The Guardian Filling in everything from one API so far.

--- js/app.js
+++ js/app.js
@@ -6,73 +6,51 @@
 
 var App = {};
 
-App.dataMain = {
-    articles : [
-        {
-            featuredImage : "images/article_placeholder_1.jpg",
-            title : "Champagne Kisses and Caviar Dreams",
-            blurb : "Lifestyle",
-            impressions : 99
-        },
-        {
-            featuredImage : "images/article_placeholder_2.jpg",
-            title : "Secrets of iOS 10",
-            blurb : "Technology",
-            impressions : 256
-        },
-        {
-            featuredImage : "images/article_placeholder_1.jpg",
-            title : "All Hail Our God-Emperor Trump",
-            blurb : "Politics and Religion",
-            impressions : 9000
-        },
-        {
-            featuredImage : "images/article_placeholder_2.jpg",
-            title : "What Do You Get When You Multiply Six by Nine?",
-            blurb : "SpaaaAaace",
-            impressions : 42
+App.data = {
+    articles : []
+};
+
+$(document).ready(function() {
+    var $popUp = $("#popUp");
+    var $main = $("#main");
+    var populateListTemplate = Handlebars.templates['articlesList'];
+    var populatePreviewTemplate = Handlebars.templates['preview'];
+
+    // let practiceQuery = "https://content.guardianapis.com/search?api-key=249f8a9f-740e-45d6-be1e-fb3baef3dfd1";
+    var practiceQuery = "https://content.guardianapis.com/us?show-most-viewed=true&show-fields=all&api-key=249f8a9f-740e-45d6-be1e-fb3baef3dfd1";
+    $.get(practiceQuery, function(r) {
+        console.log(r);
+        App.data.articles = [];
+        var sourceArray = r.response.mostViewed;
+        for (var i = 0, len = sourceArray.length; i < len; i++) {
+            let item = {
+                source: "The Guardian",
+                id: i,
+                articleid: `a${i}`,
+                previewid: `p${i}`,
+                featuredImage : sourceArray[i].fields.thumbnail,
+                title : sourceArray[i].webTitle,
+                category : sourceArray[i].sectionName,
+                impressions : sourceArray[i].fields.wordcount,
+                url: sourceArray[i].fields.shortUrl,
+                ptext: sourceArray[i].fields.trailText.replace(/\<br\>/g,".")
+            };
+            $popUp.append(populatePreviewTemplate(item));
+            $main.append(populateListTemplate(item));
+            App.data.articles.push(item);
         }
-    ]
-};
-
-App.dataPreview = {
-    title : "My Crazy Article Title",
-    preview : "Some crazy kinda article preview is gonna go here...",
-    url : "www.example.com"
-}
-
-App.buildMain = function() {
-    var articlesListTemplate = Handlebars.templates['articlesList'];
-    var output = articlesListTemplate(App.dataMain);
-    return output;
-}
-
-App.buildPreview = function() {
-    var previewTemplate = Handlebars.templates['preview'];
-    var output = previewTemplate(App.dataPreview);
-    return output;
-}
-
-App.fHideEl = function($el) {
-    return (function() {
-        $el.addClass("hidden");
+        $popUp.addClass("hidden");
     });
-}
-
-App.showPopUp = function($popUp) {
-    $popUp.append(App.buildPreview);
-    $popUp.removeClass("loader hidden");
-};
-
-$("document").ready(function() {
-    let $popUp = $("#popUp");
-
-    $popUp.removeClass("hidden");
-    $("body").append(App.buildMain);
-    window.setTimeout(App.fHideEl($popUp), 1000);
-
-    $(".article").on('click', function(event) {
-        App.showPopUp($popUp);
+    
+    // Use event delegation to handle clicks on items!
+    $main.on('click', 'article', function(event) {
+        console.log(`item ${$(this).attr('id')} clicked on`);
+        var previd = '#p' + $(this).attr('id').slice(1);
+        console.log(`Displaying preview ${previd}`)
+        var $preview = $(previd);
+        $("#popUp > div").addClass('hidden');
+        $preview.removeClass('hidden');
+        $popUp.removeClass("loader hidden");
     });
 
     $(".closePopUp").on('click', function(event) {
