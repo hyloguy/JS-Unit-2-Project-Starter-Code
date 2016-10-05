abec50d8f609c21a942b9c91603c9f56cda3a7f1
Michael Rubinstein <michael@polyblog.com>
2016-10-05 12:08:16 -0700

    Got 3 apis working and sorting full list by date!

--- js/app.js
+++ js/app.js
@@ -13,32 +13,35 @@ App.data = {
 App.apis = [
     {
         query: "https://content.guardianapis.com/us?show-most-viewed=true&show-fields=all&api-key=249f8a9f-740e-45d6-be1e-fb3baef3dfd1",
-        fetch: function(r) {
+        fetch: function(apiIndex, r) {
             console.log(r);
             var arr = r.response.mostViewed;
             for (var i = 0, len = arr.length; i < len; i++) {
+                let dateObj = new Date(arr[i].webPublicationDate);
                 let item = {
                     source: arr[i].fields.publication,
                     id: i,
-                    articleid: `a${i}`,
-                    previewid: `p${i}`,
+                    articleid: `a_${apiIndex}_${i}`,
+                    previewid: `p_${apiIndex}_${i}`,
                     featuredImage : arr[i].fields.thumbnail,
                     title : arr[i].webTitle,
-                    category : arr[i].sectionName,
+                    category : `from ${arr[i].fields.publication} in ${arr[i].sectionName} on ${dateObj.toDateString()}`,
                     impressions : arr[i].fields.wordcount,
                     url: arr[i].fields.shortUrl,
-                    ptext: arr[i].fields.trailText.replace(/\<br\>/g,".")
+                    ptext: arr[i].fields.trailText.replace(/\<br\>/g,"."),
+                    pubdate: dateObj
                 };
                 App.data.articles.push(item);
             }
         }
     },
     {
-        query: "https://newsapi.org/v1/articles?source=hacker-news&sortBy=latest&apiKey=1bc3969ac33a44e1be2cb27c03dc315f",
-        fetch: function(r) {
+        query: "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=1bc3969ac33a44e1be2cb27c03dc315f",
+        fetch: function(apiIndex, r) {
             console.log(r);
             var arr = r.articles;
             for (var i = 0, len = arr.length; i < len; i++) {
+                let dateObj = (arr[i].publishedAt) ? new Date(arr[i].publishedAt) : new Date();
                 let item = {
                     url: arr[i].url,
                     source: (function(url) {
@@ -47,14 +50,42 @@ App.apis = [
                         return a.hostname;
                     })(arr[i].url),
                     id: i,
-                    articleid: `a${i}`,
-                    previewid: `p${i}`,
+                    articleid: `a_${apiIndex}_${i}`,
+                    previewid: `p_${apiIndex}_${i}`,
                     featuredImage : arr[i].urlToImage,
                     title : arr[i].title,
-                    category : r.source,
+                    category : `from Hacker News on ${dateObj.toDateString()}`,
                     impressions : r.status,
                     ptext: arr[i].description,
-                    pubdate: arr[i].publishedAt
+                    pubdate: dateObj
+                };
+                App.data.articles.push(item);
+            }
+        }
+    },
+    {
+        query: "https://accesscontrolalloworiginall.herokuapp.com/http://thedailywtf.com/api/articles/recent/15",
+        fetch: function(apiIndex, r) {
+            console.log(r);
+            var arr = r;
+            for (var i = 0, len = arr.length; i < len; i++) {
+                let category = (function(str) {
+                    let a = str.split(':');
+                    return ((a.length > 1) ? a[0] : "Feature Articles");
+                })(arr[i].RssTitle);
+                let dateObj = new Date(arr[i].ISODate);
+                let item = {
+                    source: "The Daily WTF",
+                    id: i,
+                    articleid: `a_${apiIndex}_${i}`,
+                    previewid: `p_${apiIndex}_${i}`,
+                    featuredImage : arr[i].Author.ImageUrl,
+                    title : arr[i].Title,
+                    category : `from The Daily WTF in ${category} on ${dateObj.toDateString()}`,
+                    impressions : arr[i].CachedCommentCount,
+                    url: arr[i].Url,
+                    ptext: jQuery('<div></div>').html(arr[i].SummaryHtml).text(),
+                    pubdate: dateObj
                 };
                 App.data.articles.push(item);
             }
@@ -69,9 +100,21 @@ $(document).ready(function() {
     var populateListTemplate = Handlebars.templates['articlesList'];
     var populatePreviewTemplate = Handlebars.templates['preview'];
 
-    App.apis.forEach(function(apiObj) {
-        $.get(apiObj.query, apiObj.fetch).done(populateAndShowViews);
+    // We are going to send a GET request to each web API in the App.apis array.
+    // Each $.get is asynchronous, returning a jQuery "deferred object".
+    // But we don't want to populate our views with the results until ALL those requests complete,
+    // because we want to SORT the complete list of articles by date before we show it!
+    // So we use $.when to return a Promise from a combined "master" Deferred object for all of them.
+    // (cribbed from http://stackoverflow.com/questions/18424712/how-to-loop-through-ajax-requests-inside-a-jquery-when-then-statment)
+    var deferreds = [];
+    $.each(App.apis, function(index, apiObj) {
+        deferreds.push(
+            $.get(apiObj.query, function(response) {
+                apiObj.fetch(index, response);
+            })
+        );
     });
+    $.when.apply($, deferreds).then(populateAndShowViews);
 
     // Use event delegation to handle clicks on items!
     $main.on('click', 'article', function(event) {
@@ -86,10 +129,13 @@ $(document).ready(function() {
     });
 
     function populateAndShowViews() {
-        App.data.articles.forEach(function(item) {
-            $popUp.append(populatePreviewTemplate(item));
-            $main.append(populateListTemplate(item));
+        App.data.articles.sort(function(a,b) {
+            return (b.pubdate - a.pubdate);
         });
+        for (var i = 0; i < App.data.articles.length; i++) {
+            $popUp.append(populatePreviewTemplate(App.data.articles[i]));
+            $main.append(populateListTemplate(App.data.articles[i]));
+        };
         $popUp.addClass("hidden");
     }
 });
